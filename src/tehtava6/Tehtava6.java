bd17b86fb20f1310603f4a5b78bc599cb359a986
Tomi Turunen <Tomi.Turunen@edu.savonia.fi>
2020-03-11 20:49:05 +0200

    Tehty

---
+++ src/tehtava6/Tehtava6.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tehtava6;
+
+/**
+ *
+ * @author Tomi
+ */
+public class Tehtava6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LuokkaB terve = new LuokkaB();
+        LuokkaB terveMina = new LuokkaB("kaikki");
+        
+        terve.terve();
+        terveMina.terve();
+               
+    }
+    
+}
